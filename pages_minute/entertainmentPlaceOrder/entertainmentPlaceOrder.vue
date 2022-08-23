1c55b9f128600bec249b0a782433bfa4f72a8bad
wangjie <18161337090@163.com>
2022-08-23 13:30:23 +0800

    完成娱乐

---
+++ pages_minute/entertainmentPlaceOrder/entertainmentPlaceOrder.vue
@@ -0,0 +1,202 @@
+<template>
+	<view class="content">
+		<view class="mouduleBox">
+			<image src="../../static/index/menu_4.png" class="goodsImg"></image>
+			<view class="goodsR">
+				<view class="name">【双人】全天全场通玩</view>
+				<view class="tip">随时退·过期退</view>
+				<view class="priceBox">
+					<view>
+						<text class="unit">￥</text>
+						<text class="price">298</text>
+						<text class="oldPrice">￥289</text>
+					</view>
+					<view class="numBox">
+						<image src="../../static/order/jian.png"></image>
+						<input value="1" />
+						<image src="../../static/order/jia.png"></image>
+					</view>
+				</view>
+			</view>
+		</view>
+		<view class="mouduleBox">
+			<view class="telBox">
+				<view>预留手机尾号</view>
+				<input placeholder="填写手机尾号" />
+			</view>
+		</view>
+		<view class="botBox">
+			<view>
+				<text class="total">合计：</text>
+				<text class="unit">￥</text>
+				<text class="price">298</text>
+			</view>
+			<view class="pay">立即预订</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	.botBox {
+		width: 100%;
+		height: 124rpx;
+		background: #FFFFFF;
+		border-radius: 24rpx 24rpx 0px 0px;
+		position: fixed;
+		left: 0;
+		bottom: 0;
+		padding: 0 32rpx;
+		box-sizing: border-box;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+
+		.pay {
+			width: 256rpx;
+			height: 80rpx;
+			background: #0BB762;
+			border-radius: 100rpx;
+			font-weight: 500;
+			color: #FFFFFF;
+			font-size: 28rpx;
+			text-align: center;
+			line-height: 80rpx;
+		}
+
+		.total {
+			font-weight: 500;
+			color: #333333;
+			font-size: 28rpx;
+		}
+
+		.unit {
+			font-size: 28rpx;
+			color: #FE5A3D;
+			font-weight: bold;
+		}
+
+		.price {
+			font-size: 44rpx;
+			color: #FE5A3D;
+			font-weight: bold;
+		}
+	}
+
+	.mouduleBox {
+		width: 100%;
+		background: #fff;
+		border-radius: 24rpx;
+		padding: 24rpx 18rpx;
+		box-sizing: border-box;
+		display: flex;
+		align-items: center;
+		margin-bottom: 20rpx;
+
+		.telBox {
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			font-size: 28rpx;
+			font-weight: 500;
+			color: #333333;
+			width: 100%;
+
+			input {
+				text-align: right;
+			}
+		}
+
+		.goodsImg {
+			width: 148rpx;
+			height: 148rpx;
+			border-radius: 12rpx;
+		}
+
+		.goodsR {
+			display: flex;
+			flex-direction: column;
+			flex: 1;
+			margin-left: 22rpx;
+
+			.priceBox {
+				display: flex;
+				align-items: center;
+				justify-content: space-between;
+				margin-top: 20rpx;
+
+				.numBox {
+					display: flex;
+					align-items: center;
+
+					image {
+						width: 40rpx;
+						height: 40rpx;
+					}
+
+					input {
+						width: 50rpx;
+						font-weight: 500;
+						color: #333333;
+						font-size: 32rpx;
+						text-align: center;
+					}
+				}
+
+				.unit {
+					font-size: 28rpx;
+					color: #FE5A3D;
+					font-weight: bold;
+				}
+
+				.price {
+					font-size: 36rpx;
+					color: #FE5A3D;
+					font-weight: bold;
+				}
+
+				.oldPrice {
+					font-weight: 400;
+					color: #999999;
+					margin-left: 14rpx;
+					font-size: 24rpx;
+					text-decoration: line-through;
+				}
+			}
+
+			.name {
+				font-weight: 500;
+				color: #333333;
+				font-size: 32rpx;
+			}
+
+			.tip {
+				margin-top: 12rpx;
+				font-weight: 400;
+				color: #333333;
+				font-size: 24rpx;
+			}
+		}
+	}
+
+	.content {
+		padding: 24rpx;
+		box-sizing: border-box;
+	}
+
+	page {
+		background: rgba(244, 244, 244, 1);
+	}
+</style>
