34f97b3109ce8237bcd5eeba30307dabf4c50542
wangjie <18161337090@163.com>
2022-09-08 21:58:26 +0800

    附近景点

--- compontents/NearbyPlay.vue
+++ compontents/NearbyPlay.vue
@@ -1,10 +1,10 @@
 <template>
 	<view class="mouduleBox">
-		<view class="title">附近吃喝玩乐</view>
-		<view class="typeBox">
+		<view class="title"><view></view>附近景区推荐</view>
+		<!-- <view class="typeBox">
 			<view v-for="(item,index) in typeList" :key="index" :class="index==curt?'active':''"
 				@click="changeType(index)">{{item}}</view>
-		</view>
+		</view> -->
 		<view class="listBox" v-for="(item,index) in list" :key="index" @click="toDetail(item.id)">
 			<image :src="item.coverImg" class="img"></image>
 			<view class="goodsInfo">
@@ -20,10 +20,10 @@
 
 <script>
 	import {
-		soptList,
-		diningList,
-		hotelList,
-		mentList
+		mapState
+	} from 'vuex'
+	import {
+		soptList
 	} from '@/api/parktour.js';
 	export default {
 		data() {
@@ -32,7 +32,8 @@
 				curt: 0,
 				listQuery: {
 					page: 1,
-					pageSize: 10
+					pageSize: 10,
+					
 				},
 				list: []
 			}
@@ -40,52 +41,60 @@
 		created() {
 			this.getList()
 		},
+		computed: mapState([ 'location']),
 		methods: {
 			toDetail(id) {
-				switch (this.curt) {
-					case 0:
-						uni.redirectTo({
-							url: `/pages_minute/parktourDetail/parktourDetail?id=${id}`
-						})
-						break;
-					case 1:
-						uni.redirectTo({
-							url: `/pages_minute/diningDetail/diningDetail?id=${id}`
-						})
-						break;
-					case 2:
-						uni.redirectTo({
-							url: `/pages_minute/hotelDetail/hotelDetail?id=${id}`
-						})
-						break;
-					case 3:
-						uni.redirectTo({
-							url: `/pages_minute/entertainmentDetail/entertainmentDetail?id=${id}`
-						})
-				}
+				uni.redirectTo({
+					url: `/pages_minute/parktourDetail/parktourDetail?id=${id}`
+				})
+				// switch (this.curt) {
+				// 	case 0:
+				// 		uni.redirectTo({
+				// 			url: `/pages_minute/parktourDetail/parktourDetail?id=${id}`
+				// 		})
+				// 		break;
+				// 	case 1:
+				// 		uni.redirectTo({
+				// 			url: `/pages_minute/diningDetail/diningDetail?id=${id}`
+				// 		})
+				// 		break;
+				// 	case 2:
+				// 		uni.redirectTo({
+				// 			url: `/pages_minute/hotelDetail/hotelDetail?id=${id}`
+				// 		})
+				// 		break;
+				// 	case 3:
+				// 		uni.redirectTo({
+				// 			url: `/pages_minute/entertainmentDetail/entertainmentDetail?id=${id}`
+				// 		})
+				// }
 			},
 			async getList() {
-				if (this.curt === 0) {
 					const {
 						data
-					} = await soptList(this.listQuery)
+					} = await soptList({...this.location,...this.listQuery})
 					this.list = data.records
-				} else if (this.curt == 1) {
-					const {
-						data
-					} = await diningList(this.listQuery)
-					this.list = data.records
-				} else if (this.curt == 2) {
-					const {
-						data
-					} = await hotelList(this.listQuery)
-					this.list = data.records
-				} else if (this.curt == 3) {
-					const {
-						data
-					} = await mentList(this.listQuery)
-					this.list = data.records
-				}
+				// if (this.curt === 0) {
+				// 	const {
+				// 		data
+				// 	} = await soptList(this.listQuery)
+				// 	this.list = data.records
+				// } else if (this.curt == 1) {
+				// 	const {
+				// 		data
+				// 	} = await diningList(this.listQuery)
+				// 	this.list = data.records
+				// } else if (this.curt == 2) {
+				// 	const {
+				// 		data
+				// 	} = await hotelList(this.listQuery)
+				// 	this.list = data.records
+				// } else if (this.curt == 3) {
+				// 	const {
+				// 		data
+				// 	} = await mentList(this.listQuery)
+				// 	this.list = data.records
+				// }
 			},
 			changeType(index) {
 				this.curt = index
@@ -122,13 +131,12 @@
 
 					view {
 						height: 46rpx;
-						background: #EDF8EF;
+						background: #EEEEEE;
 						border-radius: 8rpx;
-						border: 1px solid #08B761;
 						padding: 0 16rpx;
 						line-height: 46rpx;
 						font-weight: 400;
-						color: #08B761;
+						color: #999999;
 						font-size: 24rpx;
 						margin-right: 16rpx;
 					}
@@ -187,8 +195,16 @@
 		.title {
 			font-weight: 500;
 			color: #333333;
-			font-size: 30rpx;
+			font-size: 32rpx;
 			margin-bottom: 24rpx;
+			display: flex;
+			align-items: center;
+			view{
+				width: 6rpx;
+				height: 30rpx;
+				background: linear-gradient(180deg, #BDE1CF 0%, #03B85F 100%);
+				margin-right:12rpx ;
+			}
 		}
 	}
 </style>
